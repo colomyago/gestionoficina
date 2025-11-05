e41f5e7e4dc986e97588297e04995e4bd6c7da17
iHiting <gastonheinz88@gmail.com>
2025-11-05 20:28:15 -0300

    feature: fav ico temporal

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -45,6 +45,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->brandLogo(asset('images/logo.svg'))
             ->brandLogoHeight("7rem")
+            ->favicon(asset('images/favico.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
